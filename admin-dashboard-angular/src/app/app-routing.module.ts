03a41e9c8720585af367eb8a0a94613b5b7672a1
shady00s <ssk2310@hotmail.com>
2022-07-11 13:31:34 +0200

    register validation

--- admin-dashboard-angular/src/app/app-routing.module.ts
+++ admin-dashboard-angular/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IntroPageComponent } from './component/intro-page/intro-page.component';
+import { LoginComponent } from './component/login/login.component';
+import { RegistrationComponent } from './component/registration/registration.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:IntroPageComponent},
+  {path:'log-in',component:LoginComponent},
+  {path:'registration',component:RegistrationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
